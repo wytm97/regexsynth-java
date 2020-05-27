2da02d516276112b5c80e38b4f5642dc19e2fe7b
yazeen <wytm97@gmail.com>
2020-05-27 08:47:06 +0530

    updated test case for trie expression

--- src/test/java/dev/yasint/regexsynth/synthesis/TrieExpressionTest.java
+++ src/test/java/dev/yasint/regexsynth/synthesis/TrieExpressionTest.java
@@ -7,20 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public final class TrieExpressionTest {
 
     @Test
-    public void shouldHandleUnicodeCharactersProperly() {
-
-        final String s1 = "🌚💀 Zen";
-        final String s2 = "🌚💀 Zero";
-
+    public void shouldMinimizeString() {
         TrieExpression trieExpression = new TrieExpression();
-        trieExpression.insert(s1);
-        trieExpression.insert(s2);
-
-        assertEquals(
-                trieExpression.toRegex().toString(),
-                "\uD83C\uDF1A\uD83D\uDC80 Ze(?:ro|n)"
-        );
-
+        trieExpression.insert("Apple");
+        trieExpression.insert("Application");
+        trieExpression.insert("Appeal");
+        assertEquals(trieExpression.toRegex().toString(), "App(?:eal|l(?:ication|e))");
     }
 
 }
